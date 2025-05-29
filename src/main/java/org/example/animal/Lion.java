ce1c1ed92d4cf80b3f9ca235022d3abb3422b579
aniket sahu <sahu.external7@infineon.com>
2025-05-29 08:02:14 +0530

    added missing closing curly braces

--- src/main/java/org/example/animal/Lion.java
+++ src/main/java/org/example/animal/Lion.java
@@ -4,8 +4,11 @@ public class Lion extends Animal{
     public void updateSound(){
         sounds = "roar";
     }
+    public Lion (String name){
+        //this.name = name;
+        super(name);
+    }
     public void sound() {
         System.out.println("My sound is " + sounds);
     }
-
 }
