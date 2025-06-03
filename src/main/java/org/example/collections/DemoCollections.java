b7a86c01468fd6e4be6a57e65460281830cb8755
Jamal Lee <nlamaj@gmail.com>
2025-06-02 21:34:22 -0400

    5/58/25 Error

---
+++ src/main/java/org/example/collections/DemoCollections.java
@@ -0,0 +1,15 @@
+package org.example.collections;
+
+import java.util.ArrayList;
+
+public class DemoCollections {
+
+    public static void main(String[] args) {
+        String[] demoStringArray = new String[]{"aniket","sage","errol","T"};
+        demoStringArray[0] = "malia";
+        //demoStringArray[6] = "malia";
+
+        ArrayList myFirstArrayList = new ArrayList<>();
+        myFirstArrayList.add("aniket");
+        }
+    }
